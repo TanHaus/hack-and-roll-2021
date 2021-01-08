a202bd321462c7fb2924c1234c85b634f168b5cf
picasdan9 <picasdan99@gmail.com>
2021-01-09 03:12:09 +0800

    Viz directly from tone.js

--- node-app/public/js/sketch.js
+++ node-app/public/js/sketch.js
@@ -1,62 +1,72 @@
-// let rootNode;
-// let depth;
-
-// const nodes = [],
-// 			rt3 = Math.sqrt(3);
-
-
-let objects = []
-let mic, fft;
-let dataDiv;
+let synth, analyser, pattern;
 
 function setup() {
-	// createCanvas(windowWidth, screen.availHeight, WEBGL);
-	createCanvas(windowWidth, windowHeight);
-  // setAttributes('antialias', true);
-	// createEasyCam({distance:260});
-	// // suppress right-click context menu
-  // document.oncontextmenu = function() { return false; };
-	// strokeWeight(2);
+	synth = new Tone.PolySynth({
+		// "volume": -10,
+		"envelope": {
+			"attack": 0.5,
+			"decay": 0,
+			"sustain": 0.3,
+			"release": 0,
+			}
+	}).toMaster();
 	
-	// rootNode = new Node(0, 0, 0, 1);
-	// nodes.push(rootNode);
-	// rootNode.addChildren(6);
-
-
-
+	synth.set({"oscillator": {
+						"type": "sine" 
+						}
+	});
 	
+	pattern = new Tone.Pattern(function(time, note){
+		synth.triggerAttackRelease(note, "4n");
+		}, ["C4", "D4", "E4", "A3"], "upDown");
+		//https://tonejs.github.io/docs/r12/CtrlPattern
+	
+	pattern.loop = true;
+	pattern.interval = "8n";
+	
+	analyser = new AudioEnergy();
+	synth.connect(analyser);
+	synth.connect(Tone.Master);
+	
+	Tone.Transport.start();
+	pattern.start(0);
 
-  mic = new p5.AudioIn();
-  mic.start();
-  fft = new p5.FFT();
-  fft.setInput(mic);
 
+
+  createCanvas(710, 400);
+  noFill();
 }
 
 function draw() {
-	background(255);
-	// rotateY(frameCount*.004);
-	// if (frameCount % 360 === 60) rootNode.stimulate();
-	// rootNode.draw();
+	background(0);
 
+	const dim = Math.min(width, height);
 
-	let spectrum = fft.analyze();
-	// console.log(spectrum)
-	textSize(32);
-	text('word', 10, 30);
-	fill(0, 102, 153);
-	text(JSON.stringify(spectrum), 10, 60);
-	fill(0, 102, 153, 51);
-	text('word', 10, 90);
-	beginShape();
-  for (i = 0; i < spectrum.length; i++) {
-    vertex(i, map(spectrum[i], 0, 255, height, 0));
+	strokeWeight(dim * 0.0175);
+  stroke(255);
+  noFill();
+	
+	analyser.update(deltaTime);
+
+	strokeWeight(dim * 0.0025);
+  stroke(255);
+  noFill();
+
+  beginShape();
+  const energies = analyser.getEnergyBins();
+
+  for (let i = 0; i < energies.length; i++) {
+    const energy = energies[i];
+    const x = map(i, 0, energies.length - 1, 0, width);
+    const offset = map(energy, -100, -30, 0, 1);
+    const y = height - offset * height;
+    vertex(x, y);
   }
   endShape();
 }
 
-function touchStarted() {
-  if (getAudioContext().state !== 'running') {
-    getAudioContext().resume();
-	}
+
+function updateMusic(data) {
+	// update music with new data
+	console.log(data);
 }
